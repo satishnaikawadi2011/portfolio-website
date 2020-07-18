1734c6d2f7d1b721ee40ea8845ddc726365b71a5
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2020-07-18 15:59:43 +0530

    third commit completed animation of menu button

---
+++ dist/js/main.js
@@ -0,0 +1,28 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+
+const navItems = document.querySelectorAll('.nav-item');
+
+let showMenu = false;
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+	if (!showMenu) {
+		menuBtn.classList.add('close');
+		menuNav.classList.add('show');
+		menuBranding.classList.add('show');
+		menu.classList.add('show');
+		navItems.forEach((item) => item.classList.add('show'));
+		showMenu = true;
+	}
+	else {
+		menuBtn.classList.remove('close');
+		menuNav.classList.remove('show');
+		menuBranding.classList.remove('show');
+		menu.classList.remove('show');
+		navItems.forEach((item) => item.classList.remove('show'));
+		showMenu = false;
+	}
+}
